a1416bd389623373a9e74fa46cf1d99767ced9b4
MarianaSko <maryana.skorynovych@gmail.com>
2023-12-14 15:07:52 +0200

    module5/less-2

--- src/pages/UserDetailsPage.jsx
+++ src/pages/UserDetailsPage.jsx
@@ -2,43 +2,51 @@ import { getUser } from 'API/user'
 import { Loader } from 'components/Loader/Loader'
 import { Notify } from 'notiflix'
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Navigate, useLocation, useNavigate, useParams } from 'react-router-dom'
 
 const UserDetailsPage = () => {
-    const {id} = useParams()
+  const { id } = useParams()
 
-    const [user, setUser] = useState(null)
-    const [isLoading, setIsLoading] = useState(false)
+  const [user, setUser] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
 
+  const location = useLocation()
 
-    useEffect(()=>{
-        async function fetchUserById(){
-            try {
-                setIsLoading(true)
-                const dataUser = await getUser(id)
-                setUser(dataUser)
-                setIsLoading(false)
+  const navigate = useNavigate()
 
-            } catch (error) {
-                Notify.failure(error.message)
-            }
-            
-        }
-        fetchUserById()
+  useEffect(() => {
+    async function fetchUserById() {
+      try {
+        setIsLoading(true)
+        const dataUser = await getUser(id)
+        setUser(dataUser)
+        setIsLoading(false)
+      } catch (error) {
+        Notify.failure(error.message)
+      }
+    }
+    fetchUserById()
+  }, [id])
 
-    },[id])
-    
+  const handleGoBack = () => {
+    navigate(location.state ?? '/search')
+  }
+
   return (
     <>
-    {isLoading && <Loader/>}
-    {user && 
-    <ul>
-        <li>Username - {user.username}</li>
-        <li>Age - {user.age}</li>
-        <li>Email - {user.email}</li>
-        <li>Phone -{user.phone}</li>
-    </ul>}
-    
+      {/* <Navigate to="/" /> */}
+      {isLoading && <Loader />}
+      {user && (
+        <div>
+          <ul>
+            <li>Username - {user.username}</li>
+            <li>Age - {user.age}</li>
+            <li>Email - {user.email}</li>
+            <li>Phone -{user.phone}</li>
+          </ul>
+          <button onClick={handleGoBack}>Go back</button>
+        </div>
+      )}
     </>
   )
 }
